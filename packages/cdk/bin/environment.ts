b5424aefde6d6bcddb6088b14047b3c9eec0b905
yosse95ai <karateyosi@gmail.com>
2025-12-15 16:21:02 +0000

    🔧 開発環境の設定を更新

--- packages/cdk/bin/environment.ts
+++ packages/cdk/bin/environment.ts
@@ -1,73 +1,83 @@
-export interface EnvironmentProps {
+import { z } from 'zod';
+
+export const baseSchema = z.object({
   /**
    * The AWS region where you want to deploy this system.
    * @example 'us-east-1'
    */
-  awsRegion: string;
+  awsRegion: z.string(),
 
   /**
    * You need to explicitly set AWS account ID when you look up an existing VPC or set a custom domain name.
    * @example '123456789012'
    */
-  awsAccount: string;
+  awsAccount: z.string(),
 
   /**
    * Unique name for s3 bucket
    * @example 's3-asr-bucket'
    */
-  bucketName: string;
+  bucketName: z.string(),
 
   /**
    * CIDR of API VPC
    * @example '10.0.0.0/16'
    */
-  apiVpcCidr: string;
+  apiVpcCidr: z.string(),
 
+  /**
+   * CIDR of API VPC
+   * @example '24'
+   */
+  apiVpcSubnetCidr: z.number(),
+  
   /**
    * CIDR of on-premise
    * @example '10.1.0.0/16'
    */
-  onpremiseCidr: string;
+  onpremiseCidr: z.string(),
 
   /**
    * True if you want to enable debug mode.
    * @example false
    */
-  debugMode: boolean;
+  debugMode: z.boolean(),
 
   /**
    * True if you want to setup dify packages and first deployment.
    * @example false
    */
-  difySetup: boolean;
+  difySetup: z.boolean(),
 
   /**
    * True if you want to use AWS Transcribe　to use transcription usecase
    * @example false
    */
-  useTranscribe: boolean;
+  useTranscribe: z.boolean(),
 
   /**
    * True if you want to use Bedrock Anget　to use agent usecase
    * @example false
    */
-  useBedrockAgents: boolean;
+  useBedrockAgents: z.boolean(),
   
   /**
    * True if you want to use S3 from on-premise directly
    * @example false
    */
-  useS3OnpremDirectly: boolean;
+  useS3OnpremDirectly: z.boolean(),
 
   /**
    * True is you want to use Route 53 resolver inbound endpoint
    * @example true
    */
-  useR53ResolverEndpoint: boolean;
+  useR53ResolverEndpoint: z.boolean(),
 
   /**
    * True if you want to deploy Internal NLB
    * @example false
    */
-  useInternalNlb: boolean;
-}
+  useInternalNlb: z.boolean()
+});
+
+export type EnvironmentProps = z.infer<typeof baseSchema>;
